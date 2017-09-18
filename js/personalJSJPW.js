a68e0f6cdcce418214cca371d17d264f236398d0
Josh Wood <wood2jp2@gmail.com>
2017-09-17 22:53:35 -0400

    pretty much done with projects page, adding resume page and need contact

--- js/personalJSJPW.js
+++ js/personalJSJPW.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
   $('.slider').slider();
   $('.parallax').parallax();
+  $('.carousel-slider').carousel();
+
+  var carouselLink = $('<a>')
+
+  $('.fixed-action-btn').on('click', function() {
+    $('.fixed-action-btn').openFAB();
+  });
+
+
+  // $(carouselLink).on('click', function() {
+  //   if (this.attr('id') === 'curb') {
+  //     window.location.href = "https://curb-your-comment.herokuapp.com/home"
+  //   } else if (this.attr('id') === 'burger') {
+  //     window.location.href = "www.google.com"
+  //   };
+  // })
 });
