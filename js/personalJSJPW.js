9ac38adab85c8a378c02f853df79695a980c861a
Josh Wood <wood2jp2@gmail.com>
2017-09-18 13:39:16 -0400

    navbar and contact page in progress

--- js/personalJSJPW.js
+++ js/personalJSJPW.js
@@ -2,19 +2,12 @@ $(document).ready(function() {
   $('.slider').slider();
   $('.parallax').parallax();
   $('.carousel-slider').carousel();
+  $('.button-collapse').sideNav('show');
+  $(".button-collapse").sideNav();
 
   var carouselLink = $('<a>')
 
   $('.fixed-action-btn').on('click', function() {
     $('.fixed-action-btn').openFAB();
   });
-
-
-  // $(carouselLink).on('click', function() {
-  //   if (this.attr('id') === 'curb') {
-  //     window.location.href = "https://curb-your-comment.herokuapp.com/home"
-  //   } else if (this.attr('id') === 'burger') {
-  //     window.location.href = "www.google.com"
-  //   };
-  // })
 });
